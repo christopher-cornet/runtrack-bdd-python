3c04eedef3eeb35d1c24b2422ad98cfc3ad48a5c
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-03-13 18:46:07 +0100

    Delete job13.sql

--- jour01/job14/job13.sql
+++
@@ -1,7 +0,0 @@
-select * from etudiants where age BETWEEN 18 and 25;
-
-id	nom	prenom	age	email
-1	Spaghetti	Betty	23	betty.spaghetti@laplateforme.io
-3	Doe	John	18	john.doe@laplateforme.io
-5	Dupuis	Gertrude	20	gertrude.dupuis@laplateforme.io
-6	Dupuis	Martin	18	martin.dupuis@laplateforme.io
